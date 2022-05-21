f163999806a946639a2731a16e5d348f1e1a1b46
zukxu <zukxu520@163.com>
2022-05-21 16:41:31 +0800

    fix(*): 全局异常处理器 fix

--- cloud-common/cloud-common-core/src/main/java/com/zukxu/common/core/response/RRAdvice.java
+++ cloud-common/cloud-common-core/src/main/java/com/zukxu/common/core/response/RRAdvice.java
@@ -1,22 +1,15 @@
 package com.zukxu.common.core.response;
 
-import com.zukxu.common.core.exception.BaseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.MethodParameter;
 import org.springframework.core.annotation.AnnotatedElementUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-import org.springframework.web.util.WebUtils;
 
 /**
  * <p>
@@ -59,53 +52,6 @@ public class RRAdvice implements ResponseBodyAdvice<Object> {
             return body;
         }
         return R.ok(body);
-    }
-
-
-    /**
-     * 提供对标准Spring MVC异常的处理
-     *
-     * @param ex      the target exception
-     * @param request the current request
-     */
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<R<?>> exceptionHandler(Exception ex, WebRequest request) {
-        log.error("异常处理===>::: {}", ex.getMessage());
-        HttpHeaders headers = new HttpHeaders();
-        if(ex instanceof BaseException) {
-            return this.handleResultException((BaseException) ex, headers, request);
-        }
-        // TODO: 2019/10/05 galaxy 这里可以自定义其他的异常拦截
-        return this.handleException(ex, headers, request);
-    }
-
-    /** 对ResultException类返回返回结果的处理 */
-    protected ResponseEntity<R<?>> handleResultException(BaseException ex, HttpHeaders headers, WebRequest request) {
-        R<?> body = R.fail(ex.getModule() + "===>" + ex.getMessage()+"===>"+ex.getDefaultMessage());
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        return this.handleExceptionInternal(ex, body, headers, status, request);
-    }
-
-    /** 异常类的统一处理 */
-    protected ResponseEntity<R<?>> handleException(Exception ex, HttpHeaders headers, WebRequest request) {
-        R<?> body = R.fail(ex.getMessage());
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        return this.handleExceptionInternal(ex, body, headers, status, request);
-    }
-
-    /**
-     * org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler#handleExceptionInternal(java.lang.Exception, java.lang.Object, org.springframework.http.HttpHeaders, org.springframework.http.HttpStatus, org.springframework.web.context.request.WebRequest)
-     * <p>
-     * A single place to customize the response body of all exception types.
-     * <p>The default implementation sets the {@link WebUtils#ERROR_EXCEPTION_ATTRIBUTE}
-     * request attribute and creates a {@link ResponseEntity} from the given
-     * body, headers, and status.
-     */
-    protected ResponseEntity<R<?>> handleExceptionInternal(Exception ex, R<?> body, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        if(HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
-            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
-        }
-        return new ResponseEntity<>(body, headers, status);
     }
 
 }
--- cloud-common/cloud-common-feign/src/main/java/com/zukxu/common/feign/sentinel/handler/GlobalExceptionHandler.java
+++ cloud-common/cloud-common-feign/src/main/java/com/zukxu/common/feign/sentinel/handler/GlobalExceptionHandler.java
@@ -1,23 +1,10 @@
-/*
- * Copyright (c) 2020 pig4cloud Authors. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.zukxu.common.feign.sentinel.handler;
 
 import com.alibaba.csp.sentinel.Tracer;
+import com.zukxu.common.core.exception.BaseException;
+import com.zukxu.common.core.exception.BusinessException;
 import com.zukxu.common.core.response.R;
+import com.zukxu.common.core.response.RStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.http.HttpStatus;
@@ -28,15 +15,19 @@ import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 
 /**
  * <p>
- * 全局异常处理器结合sentinel 全局异常处理器不能作用在 oauth server https://gitee.com/log4j/pig/issues/I1M2TJ
+ * 全局异常处理器结合sentinel 全局异常处理器不能作用在 oauth server
+ * <a href="https://gitee.com/log4j/pig/issues/I1M2TJ">https://gitee.com/log4j/pig/issues/I1M2TJ</a>
  * </p>
  *
  * @author xupu
@@ -47,79 +38,109 @@ import java.util.List;
 @ConditionalOnExpression("!'${security.oauth2.client.clientId}'.isEmpty()")
 public class GlobalExceptionHandler {
 
-	/**
-	 * 全局异常.
-	 *
-	 * @param e the e
-	 * @return R
-	 */
-	@ExceptionHandler(Exception.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public R<?> handleGlobalException(Exception e) {
-		log.error("全局异常信息 ex={}", e.getMessage(), e);
-		// 业务异常交由 sentinel 记录
-		Tracer.trace(e);
-		return R.fail(e.getLocalizedMessage());
-	}
+    /**
+     * 全局异常
+     *
+     * @param e the e
+     *
+     * @return R
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public R<?> handleGlobalException(Exception e) {
+        log.error("系统异常==>{}:{}", Arrays.stream(e.getStackTrace()).toArray()[0].toString(), e.getMessage());
+        // 业务异常交由 sentinel 记录
+        Tracer.trace(e);
+        return R.fail(e.getLocalizedMessage());
+    }
 
-	/**
-	 * 处理业务校验过程中碰到的非法参数异常 该异常基本由{@link Assert}抛出
-	 *
-	 * @param exception 参数校验异常
-	 * @return API返回结果对象包装后的错误输出结果
-	 * @see Assert#hasLength(String, String)
-	 * @see Assert#hasText(String, String)
-	 * @see Assert#isTrue(boolean, String)
-	 * @see Assert#isNull(Object, String)
-	 * @see Assert#notNull(Object, String)
-	 */
-	@ExceptionHandler(IllegalArgumentException.class)
-	@ResponseStatus(HttpStatus.OK)
-	public R<?> handleIllegalArgumentException(IllegalArgumentException exception) {
-		log.error("非法参数,ex = {}", exception.getMessage(), exception);
-		return R.fail(exception.getMessage());
-	}
+    /**
+     * 自定义异常处理方法
+     *
+     * @param e BaseException
+     */
+    @ExceptionHandler(BaseException.class)
+    @ResponseBody
+    public R<?> handlerBaseException(BaseException e) {
+        log.error("基础系统异常==>{}:{}", Arrays.stream(e.getStackTrace()).toArray()[0].toString(), e.getMessage());
+        return R.fail().msg(e.getMessage()).code(Optional.ofNullable(e.getCode()).orElse(RStatus.FAIL.getCode()));
+    }
 
-	/**
-	 * AccessDeniedException
-	 *
-	 * @param e the e
-	 * @return R
-	 */
-	@ExceptionHandler(AccessDeniedException.class)
-	@ResponseStatus(HttpStatus.FORBIDDEN)
-	public R<?> handleAccessDeniedException(AccessDeniedException e) {
-		String msg = SpringSecurityMessageSource.getAccessor().getMessage("AbstractAccessDecisionManager.accessDenied", e.getMessage());
-		log.warn("拒绝授权异常信息 ex={}", msg);
-		return R.fail(e.getLocalizedMessage());
-	}
+    /**
+     * 自定义异常处理方法
+     *
+     * @param e BusinessException
+     */
+    @ExceptionHandler(BusinessException.class)
+    @ResponseBody
+    public R<?> handlerBusinessException(BusinessException e) {
+        log.error("业务异常==>{}:{}", Arrays.stream(e.getStackTrace()).toArray()[0].toString(), e.getMessage());
+        return R.fail().msg(e.getMessage()).code(Optional.of(e.getCode()).orElse(RStatus.FAIL.getCode()));
+    }
 
-	/**
-	 * validation Exception
-	 *
-	 * @param exception
-	 * @return R
-	 */
-	@ExceptionHandler({MethodArgumentNotValidException.class})
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public R<?> handleBodyValidException(MethodArgumentNotValidException exception) {
-		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
-		log.warn("参数绑定异常,ex = {}", fieldErrors.get(0).getDefaultMessage());
-		return R.fail(fieldErrors.get(0).getDefaultMessage());
-	}
+    /**
+     * 处理业务校验过程中碰到的非法参数异常 该异常基本由{@link Assert}抛出
+     *
+     * @param e 参数校验异常
+     *
+     * @return API返回结果对象包装后的错误输出结果
+     *
+     * @see Assert#hasLength(String, String)
+     * @see Assert#hasText(String, String)
+     * @see Assert#isTrue(boolean, String)
+     * @see Assert#isNull(Object, String)
+     * @see Assert#notNull(Object, String)
+     */
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.OK)
+    public R<?> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.error("非法参数,ex==>{}:{}", Arrays.stream(e.getStackTrace()).toArray()[0].toString(), e.getMessage());
+        return R.fail(e.getMessage());
+    }
 
-	/**
-	 * validation Exception (以form-data形式传参)
-	 *
-	 * @param exception
-	 * @return R
-	 */
-	@ExceptionHandler({BindException.class})
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public R<?> bindExceptionHandler(BindException exception) {
-		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
-		log.warn("参数绑定异常,ex = {}", fieldErrors.get(0).getDefaultMessage());
-		return R.fail(fieldErrors.get(0).getDefaultMessage());
-	}
+    /**
+     * AccessDeniedException
+     *
+     * @param e the e
+     *
+     * @return R
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public R<?> handleAccessDeniedException(AccessDeniedException e) {
+        String msg = SpringSecurityMessageSource.getAccessor().getMessage("AbstractAccessDecisionManager.accessDenied", e.getMessage());
+        log.warn("拒绝授权异常信息 ex={}", msg);
+        return R.fail(e.getLocalizedMessage());
+    }
+
+    /**
+     * validation Exception
+     *
+     * @param exception e
+     *
+     * @return R
+     */
+    @ExceptionHandler({ MethodArgumentNotValidException.class })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public R<?> handleBodyValidException(MethodArgumentNotValidException exception) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        log.warn("参数绑定异常,ex = {}", fieldErrors.get(0).getDefaultMessage());
+        return R.fail(fieldErrors.get(0).getDefaultMessage());
+    }
+
+    /**
+     * validation Exception (以form-data形式传参)
+     *
+     * @param exception e
+     *
+     * @return R
+     */
+    @ExceptionHandler({ BindException.class })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public R<?> bindExceptionHandler(BindException exception) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        log.warn("参数绑定异常,ex = {}", fieldErrors.get(0).getDefaultMessage());
+        return R.fail(fieldErrors.get(0).getDefaultMessage());
+    }
 
 }
